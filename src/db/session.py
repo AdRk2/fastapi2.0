65055860864278ea735e23dc07f0b3b17e3d156e
AdRk2 <ar-adam.abed@rakuten.com>
2025-05-19 11:10:55 +0900

    Configuration for database started and up

---
+++ src/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+print("Database URL is ",SQLALCHEMY_DATABASE_URL)
+
+#try-catch errrors
+try:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+except Exception as e:
+    print("Database cannot be up !",e)
+
+
+SessionLocal = sessionmaker(bind=engine)
