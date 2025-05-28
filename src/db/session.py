8dc0e4c9c3627bd2c873af2728e8512b60ec5e32
AdRk2 <ar-adam.abed@rakuten.com>
2025-05-28 13:09:19 +0900

    Update session.py to match the containerapps credentials

--- src/db/session.py
+++ src/db/session.py
@@ -1,13 +1,11 @@
 from typing import Generator
 
-from core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 
-SQLALCHEMY_DATABASE_URL ="postgresql://stg:adam@172.20.0.2:5432/blogdb"
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+#SQLALCHEMY_DATABASE_URL ="postgresql://stg:adam@db:5432/blogdb"
 
+SQLALCHEMY_DATABASE_URL = "postgresql://devadmin:ijustwanttolog2%40@d:5432/postgres"
 print("Database URL is ",SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
